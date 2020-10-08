c857daf5fe444228c43ce3272a66c0e9d00a7410
Jerald Sheets <jsheets@ssconsultinggroup.net>
2020-10-08 10:34:56 -0400

    completed service disable control items Rubocop recommendations

--- spec/acceptance/services/cis_hardening_services_svcclients_spec.rb
+++ spec/acceptance/services/cis_hardening_services_svcclients_spec.rb
@@ -1,26 +1,28 @@
 require 'spec_helper_acceptance'
 
-# Ensure NIS CLient is not installed - Section 2.3.1
-describe package('ypbind') do
-  it { is_expected.not_to be_installed }
-end
+describe 'Services to be disabled' do
+  # Ensure NIS CLient is not installed - Section 2.3.1
+  describe package('ypbind') do
+    it { is_expected.not_to be_installed }
+  end
 
-# Ensure rsh Client is not installed - Section 2.3.2
-describe package('rsh') do
-  it { is_expected.not_to be_installed}
-end
+  # Ensure rsh Client is not installed - Section 2.3.2
+  describe package('rsh') do
+    it { is_expected.not_to be_installed }
+  end
 
-# Ensure talk client is not installed - Section 2.3.3
-describe package('talk') do
-  it { is_expected.not_to be_installed }
-end
+  # Ensure talk client is not installed - Section 2.3.3
+  describe package('talk') do
+    it { is_expected.not_to be_installed }
+  end
 
-# Ensure telnet client is not installed - Section 2.3.4
-describe package('telnet') do
-  it { is_expected.not_to be_installed }
-end
+  # Ensure telnet client is not installed - Section 2.3.4
+  describe package('telnet') do
+    it { is_expected.not_to be_installed }
+  end
 
-# Ensure LDAP client is not installed - Section 2.3.5
-describe package('openldap-clients') do
-  it { is_expected.not_to be_installed }
+  # Ensure LDAP client is not installed - Section 2.3.5
+  describe package('openldap-clients') do
+    it { is_expected.not_to be_installed }
+  end
 end
