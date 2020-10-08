cbffcb96d8a4d69fb3dca2664540b902b8f97caa
Jerald Sheets <jsheets@ssconsultinggroup.net>
2020-10-08 11:39:35 -0400

    Completed Rubocop suggestions for IPv6 Hardening

--- spec/acceptance/network/cis_hardening_network_ipv6_spec.rb
+++ spec/acceptance/network/cis_hardening_network_ipv6_spec.rb
@@ -1,22 +1,23 @@
 require 'spec_helper_acceptance'
 
-describe 'cis_hardening_network_ipv6 class' do
-  context 'default parameters' do
-    it 'behaves idempotently' do
-      idempotent_apply(pp)
+describe ' IPv6 Networking' do
+  describe 'cis_hardening_network_ipv6 class' do
+    context 'default parameters' do
+      it 'behaves idempotently' do
+        idempotent_apply(pp)
+      end
     end
   end
-end
 
-# Ensure IPv6 router advertisements are not accepted - Section 3.3.1
-# Ensure IPv6 redirets are not accepted - Section 3.3.2
-describe file ( '/etc/sysctl.d/99-sysctl.conf' ) do
+  # Ensure IPv6 router advertisements are not accepted - Section 3.3.1
+  # Ensure IPv6 redirets are not accepted - Section 3.3.2
+  describe file '/etc/sysctl.d/99-sysctl.conf' do
     it { is_expected.to be_symlink }
     it { is_expected.to be_owned_by 'root' }
     it { is_expected.to be_grouped_into 'root' }
-    its (:content) { is_expected.to match (/net.ipv6.conf.all.accept_ra = 0/) }
-    its (:content) { is_expected.to match (/net.ipv6.conf.default.accept_ra = 0/) }
-    its (:content) { is_expected.to match (/net.ipv6.conf.all.accept_redirects = 0/) }
-    its (:content) { is_expected.to match (/net.ipv6.conf.default.accept_redirects = 0/) }
+    its(:content) { is_expected.to match %r{/net.ipv6.conf.all.accept_ra = 0/} }
+    its(:content) { is_expected.to match %r{/net.ipv6.conf.default.accept_ra = 0/} }
+    its(:content) { is_expected.to match %r{/net.ipv6.conf.all.accept_redirects = 0/} }
+    its(:content) { is_expected.to match %r{/net.ipv6.conf.default.accept_redirects = 0/} }
+  end
 end
-
