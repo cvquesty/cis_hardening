6996a7c504ea1b84192185b173edfa5a2aadb4af
Jerald Sheets <jsheets@ssconsultinggroup.net>
2020-10-08 11:22:55 -0400

    Completed Rubocop suggestions for TCP Wrappers tests

--- spec/acceptance/network/cis_hardening_network_tcpwrappers_spec.rb
+++ spec/acceptance/network/cis_hardening_network_tcpwrappers_spec.rb
@@ -1,22 +1,24 @@
 require 'spec_helper_acceptance'
 
-describe 'cis_hardening_network_tcpwrappers class' do
-  context 'default parameters' do
-    it 'behaves idempotently' do
-      idempotent_apply(pp)
+describe ' TCP Wrappers Settings' do
+  describe 'cis_hardening_network_tcpwrappers class' do
+    context 'default parameters' do
+      it 'behaves idempotently' do
+        idempotent_apply(pp)
+      end
     end
   end
-end
 
-# Verify TCP Wrappers are installed - Section 3.4.1
-describe package('tcp_wrappers') do
+  # Verify TCP Wrappers are installed - Section 3.4.1
+  describe package('tcp_wrappers') do
     it { is_expected.to be_installed }
-end
+  end
 
-# Ensure /etc/hosts.allow is configured - Section 3.4.2, 3.4.4
-describe file('/etc/hosts.allow') do
+  # Ensure /etc/hosts.allow is configured - Section 3.4.2, 3.4.4
+  describe file('/etc/hosts.allow') do
     it { is_expected.to be_file }
     it { is_expected.to be_owned_by 'root' }
     it { is_expected.to be_grouped_into 'root' }
     it { is_expected.to be_mode 644 }
+  end
 end
