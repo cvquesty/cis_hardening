d72ba457074185c6bc8da735df3882c8e9c28f8f
Jerald Sheets <jsheets@ssconsultinggroup.net>
2020-10-08 10:58:03 -0400

    completed Rubocop suggestions for Access Control items

--- spec/acceptance/setup/cis_hardening_setup_accessctl_spec.rb
+++ spec/acceptance/setup/cis_hardening_setup_accessctl_spec.rb
@@ -1,36 +1,38 @@
 require 'spec_helper_acceptance'
 
-# Ensure SELinux is not disabled in bootloader configuration - Section 1.6.1.1
-describe file('/etc/default/grub') do
+describe 'Access Control Settings' do
+  # Ensure SELinux is not disabled in bootloader configuration - Section 1.6.1.1
+  describe file('/etc/default/grub') do
     it { is_expected.to be_file }
     it { is_expected.to be_owned_by 'root' }
     it { is_expected.to be_grouped_into 'root' }
     it { is_expected.to be_mode 644 }
-    its(:content) { is_expected.to match (/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/) }
-    its(:content) { is_expected.to match (/GRUB_CMDLINE_LINUX="audit=1"/) }
-end
+    its(:content) { is_expected.to match %r{/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/} }
+    its(:content) { is_expected.to match %r{/GRUB_CMDLINE_LINUX="audit=1"/} }
+  end
 
-# Ensure the SELinux state is "enforcing" - Section 1.6.1.2
-describe file('/etc/selinux/config') do
+  # Ensure the SELinux state is "enforcing" - Section 1.6.1.2
+  describe file('/etc/selinux/config') do
     it { is_expected.to be_file }
     it { is_expected.to be_owned_by 'root' }
     it { is_expected.to be_grouped_into 'root' }
     it { is_expected.to be_mode 644 }
-    its(:content) { is_expected.to match (/SELINUX=enforcing/) }
-    its(:content) { is_expected.to match (/SELINUXTYPE=targeted/) }
-end
+    its(:content) { is_expected.to match %r{/SELINUX=enforcing/} }
+    its(:content) { is_expected.to match %r{/SELINUXTYPE=targeted/} }
+  end
 
-# Ensure SETroubleshoot is not installed - Section 1.6.1.4
-describe package('setroubleshoot') do
+  # Ensure SETroubleshoot is not installed - Section 1.6.1.4
+  describe package('setroubleshoot') do
     it { is_expected.not_to be_installed }
-end
+  end
 
-# Ensure MCS Translation Service is not installed - Section 1.6.1.5
-describe package('mcstrans') do
+  # Ensure MCS Translation Service is not installed - Section 1.6.1.5
+  describe package('mcstrans') do
     it { is_expected.not_to be_installed }
-end
+  end
 
-# Ensure SELinux is installed - Section 1.6.2
-describe package('libselinux') do
+  # Ensure SELinux is installed - Section 1.6.2
+  describe package('libselinux') do
     it { is_expected.to be_installed }
+  end
 end
