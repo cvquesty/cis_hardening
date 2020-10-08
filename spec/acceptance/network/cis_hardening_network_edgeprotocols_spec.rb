19c8df6685a05c69005128a84e5ba3866d8ab194
Jerald Sheets <jsheets@ssconsultinggroup.net>
2020-10-08 11:01:17 -0400

    completed Rubocop suggestions on Edge Protocols tests

--- spec/acceptance/network/cis_hardening_network_edgeprotocols_spec.rb
+++ spec/acceptance/network/cis_hardening_network_edgeprotocols_spec.rb
@@ -1,24 +1,26 @@
 require 'spec_helper_acceptance'
 
-describe 'cis_hardening_network_aedgeprotocols class' do
-  context 'default parameters' do
-    it 'behaves idempotently' do
-      idempotent_apply(pp)
+describe 'Disable edge networking protocols' do
+  describe 'cis_hardening_network_aedgeprotocols class' do
+    context 'default parameters' do
+      it 'behaves idempotently' do
+        idempotent_apply(pp)
+      end
     end
   end
-end
 
-# Ensure DCCP is disabled - Section 3.5.1
-# Ensure SCTP is disabled - Section 3.5.2
-# Ensure RDS is disabled - Section 3.5.3
-# Ensure TIPC is disabled - Section 3.5.4
-describe file('/etc/modprobe.d/CIS.conf') do
+  # Ensure DCCP is disabled - Section 3.5.1
+  # Ensure SCTP is disabled - Section 3.5.2
+  # Ensure RDS is disabled - Section 3.5.3
+  # Ensure TIPC is disabled - Section 3.5.4
+  describe file('/etc/modprobe.d/CIS.conf') do
     it { is_expected.to be_file }
     it { is_expected.to be_owned_by 'root' }
     it { is_expected.to be_grouped_into 'root' }
     it { is_expected.to be_mode 644 }
-    its(:content) { is_expected.to match (/install dccp \/bin\/true/) }
-    its(:content) { is_expected.to match (/install sctp \/bin\/true/) }
-    its(:content) { is_expected.to match (/install rds \/bin\/true/) }
-    its(:content) { is_expected.to match (/install tipc \/bin\/true/) }
+    its(:content) { is_expected.to match %r{/install dccp \/bin\/true/} }
+    its(:content) { is_expected.to match %r{/install sctp \/bin\/true/} }
+    its(:content) { is_expected.to match %r{/install rds \/bin\/true/} }
+    its(:content) { is_expected.to match %r{/install tipc \/bin\/true/} }
+  end
 end
