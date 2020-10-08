2259b8fb2b499513a2635e8c317ad007bbf4ad72
Jerald Sheets <jsheets@ssconsultinggroup.net>
2020-10-08 11:06:11 -0400

    completed Rubocop suggestions for network firewall settings tests

--- spec/acceptance/network/cis_hardening_network_firewall_spec.rb
+++ spec/acceptance/network/cis_hardening_network_firewall_spec.rb
@@ -11,7 +11,7 @@ describe 'Network Firewall Configuration' do
 
   # Ensure IPTables is installed - Section 3.6.1
   describe package('iptables') do
-      it { is_expected.to be_installed }
+    it { is_expected.to be_installed }
   end
 
   # Ensure Default deny Firewall Policy - Section 3.6.2
@@ -20,11 +20,11 @@ describe 'Network Firewall Configuration' do
   # Ensure Firewall rules exist for all open ports - Section 3.6.5
   # Ensure wireless interfaces are disabled - Section 3.7
   describe file('/etc/sysconfig/iptables') do
-      it { is_expected.to be_file }
-      it { is_expected.to be_owned_by 'root' }
-      it { is_expected.to be_grouped_into 'root' }
-      it { is_expected.to be_mode 600 }
-      its (:content) { is_expected.to match %r{/# This File Managed by Puppet. DO NOT EDIT./} }
-      its (:content) { is_expected.to match %r{/-A INPUT -p tcp --dport 22 -m state --state NEW -j ACCEPT/} }
+    it { is_expected.to be_file }
+    it { is_expected.to be_owned_by 'root' }
+    it { is_expected.to be_grouped_into 'root' }
+    it { is_expected.to be_mode 600 }
+    its(:content) { is_expected.to match %r{/# This File Managed by Puppet. DO NOT EDIT./} }
+    its(:content) { is_expected.to match %r{/-A INPUT -p tcp --dport 22 -m state --state NEW -j ACCEPT/} }
   end
 end
