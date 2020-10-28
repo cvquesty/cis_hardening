a09cc4bc6570a4c77351ecd532a730a6c1afe233
Jerald Sheets <jsheets@ssconsultinggroup.net>
2020-10-28 16:50:20 -0400

    trying new syntax to same result

--- spec/classes/maint/usergroups_spec.rb
+++ spec/classes/maint/usergroups_spec.rb
@@ -24,7 +24,7 @@ describe 'cis_hardening::maint::usergroups' do
         is_expected.to contain_exec('shadow_password_field_check').with(
           'path'    => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
           'command' => 'logger -p crit "Shadow Password Fields Empty. Check manually."',
-          'onlyif'  => '',
+          'onlyif'  => "test ! `awk -F: '($2 == "")' /etc/shadow`",
         )
       }
 
