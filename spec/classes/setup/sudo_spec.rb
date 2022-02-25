230bf854172e60c57f823f45d9d836649f59acf1
Jerald Sheets <jsheets@ssconsultinggroup.net>
2022-02-25 12:31:27 -0500

    correcting rubocop issues with cis_hardening module

--- spec/classes/setup/sudo_spec.rb
+++ spec/classes/setup/sudo_spec.rb
@@ -12,41 +12,41 @@ describe 'cis_hardening::setup::sudo' do
         is_expected.to contain_class('cis_hardening::setup::sudo')
       }
 
-        # Section 1.3 - Configure sudo
+      # Section 1.3 - Configure sudo
 
-        # Ensure sudo is installed - Section 1.3.1
-        it {
-          is_expected.to contain_package('sudo').with(
-            'ensure' => 'installed',
-          )
-        }
+      # Ensure sudo is installed - Section 1.3.1
+      it {
+        is_expected.to contain_package('sudo').with(
+          'ensure' => 'installed',
+        )
+      }
 
-        # Ensure sudo commands use pty - Section 1.3.2
-        it {
-          is_expected.to contain_file('/etc/sudoers.d/cis_sudoers_defaults.conf').with(
-            'ensure' => 'present',
-            'owner'  => 'root',
-            'group'  => 'root',
-            'mode'   => '0440',
-          )
-        }
+      # Ensure sudo commands use pty - Section 1.3.2
+      it {
+        is_expected.to contain_file('/etc/sudoers.d/cis_sudoers_defaults.conf').with(
+          'ensure' => 'present',
+          'owner'  => 'root',
+          'group'  => 'root',
+          'mode'   => '0440',
+        )
+      }
 
-        it {
-          is_expected.to contain_file_line('defaults_pty').with(
-            'ensure'  => 'present',
-            'path'    => '/etc/sudoers.d/cis_sudoers_defaults.conf',
-            'line'    => 'Defaults use_pty',
-          ).that_requires('File[/etc/sudoers.d/cis_sudoers_defaults.conf]')
-        }
+      it {
+        is_expected.to contain_file_line('defaults_pty').with(
+          'ensure'  => 'present',
+          'path'    => '/etc/sudoers.d/cis_sudoers_defaults.conf',
+          'line'    => 'Defaults use_pty',
+        ).that_requires('File[/etc/sudoers.d/cis_sudoers_defaults.conf]')
+      }
 
-        # Ensure sudo log file exists - Section 1.3.3
-        it {
-          is_expected.to contain_file_line('defaults_sudo_logfile').with(
-            'ensure' => 'present',
-            'path'   => '/etc/sudoers.d/cis_sudoers_defaults.conf',
-            'line'   => 'Defaults logfile="/var/log/sudo.log"',
-          ).that_requires('File[/etc/sudoers.d/cis_sudoers_defaults.conf]')
-        }
+      # Ensure sudo log file exists - Section 1.3.3
+      it {
+        is_expected.to contain_file_line('defaults_sudo_logfile').with(
+          'ensure' => 'present',
+          'path'   => '/etc/sudoers.d/cis_sudoers_defaults.conf',
+          'line'   => 'Defaults logfile="/var/log/sudo.log"',
+        ).that_requires('File[/etc/sudoers.d/cis_sudoers_defaults.conf]')
+      }
 
       # Ensure manifest compiles with all dependencies
       it {
